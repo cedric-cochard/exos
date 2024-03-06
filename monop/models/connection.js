c9eabf7b4ed24a398d7660239b9cc0bbf9fd7035
cedric-cochard <c.jc@live.fr>
2024-03-06 18:16:19 +0100

    add env variable

--- monop/models/connection.js
+++ monop/models/connection.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://admin:fDGe3xUACUsaMIrA@cluster0.k1mrads.mongodb.net/monop";
+const connectionString = process.env.CONNECTION_STRING;
+
+console.log("connectionString:", connectionString);
 
 mongoose
+  // @ts-ignore
   .connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log("Database Connected"))
   .catch((error) => console.log(error, "error"));
--- monop/script.js
+++ monop/script.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 require("./models/connection");
 
 const DataExcel = require("./models/dataExcel");
 
 const updateData = () => {
   const newDataExcel = new DataExcel({
-    codeOsp: "coucou",
+    codeOsp: "coucou2",
     irefc: 122345,
     date: "06/03/2024",
   });
